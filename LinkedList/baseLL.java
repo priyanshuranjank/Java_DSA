8c3489614f340abeda410214b7efe4bf5b1924b7
priyanshu3492 <priyanshuranjan1r@gmail.com>
2025-03-13 01:41:30 +0530

    ll

--- LinkedList/baseLL.java
+++ LinkedList/baseLL.java
@@ -4,8 +4,8 @@ public class baseLL {
 
     baseLL ll = new baseLL(); //object
 
-    ll.head = new Node(1);
-    ll.head.next = new Node(2);
+    ll.addFirst(2);
+    ll.addFirst(1);
     
         
 
@@ -25,4 +25,19 @@ public class baseLL {
 
     public static Node head; //head node defined as property
     public static Node tail; //tail node
+
+    //Methods 
+    //Add first 
+    public void addFirst(int data){
+        Node newNode = new Node(data); //1. cretaed new node
+        
+        if(head == null){ // there is nothing then head and tail will be same
+            head = tail = newNode;
+        }
+
+        newNode.next = head; // updating next of new node(first) by location value of old head
+
+        head = newNode; // assigning new node as head, newnode is the first element now so it should be head
+    }
+
 }
