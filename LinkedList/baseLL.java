4236fa5fb7eed0bd1cc54cc5ba8de406aa4cd377
priyanshu3492 <priyanshuranjan1r@gmail.com>
2025-03-13 02:06:46 +0530

    LL

--- LinkedList/baseLL.java
+++ LinkedList/baseLL.java
@@ -6,9 +6,10 @@ public class baseLL {
 
     ll.addFirst(2);
     ll.addFirst(1);
+    ll.addLast(30);
+    ll.addLast(40);
     
-        
-
+    ll.print();
         
     }
 
@@ -30,9 +31,10 @@ public class baseLL {
     //Add first 
     public void addFirst(int data){
         Node newNode = new Node(data); //1. cretaed new node
-        
+
         if(head == null){ // there is nothing then head and tail will be same
             head = tail = newNode;
+            return;
         }
 
         newNode.next = head; // updating next of new node(first) by location value of old head
@@ -40,4 +42,28 @@ public class baseLL {
         head = newNode; // assigning new node as head, newnode is the first element now so it should be head
     }
 
+    //Add last
+    public void addLast(int data){
+        Node tailNewNode = new Node(data); // created new node to add in last
+
+        if(head==null){  // tail==null can also be written
+            head = tail = tailNewNode;
+            return;
+        }
+
+        tail.next = tailNewNode; // upating next of previous tail to new tail node
+
+        tail = tailNewNode; // updating tail to newTailNode
+    }
+
+    //Print LL
+    public void print(){
+        if(head==null){System.out.println("-1");}
+
+        Node temp = head; //saving head node in a temp node
+        while(temp != null){
+            System.out.print(temp.data +" ");
+            temp = temp.next; // going to 2nd coming node by address with next
+        }
+    }
 }
