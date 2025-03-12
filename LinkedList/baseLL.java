e7310d01ae3547d235bf1da8a97ce891e04f863d
priyanshu3492 <priyanshuranjan1r@gmail.com>
2025-03-12 22:11:31 +0530

    ll

--- LinkedList/baseLL.java
+++ LinkedList/baseLL.java
@@ -1,28 +1,28 @@
 public class baseLL {
+
     public static void main(String[] args) {
 
+    baseLL ll = new baseLL(); //object
+
+    ll.head = new Node(1);
+    ll.head.next = new Node(2);
+    
         
 
         
     }
+
+// Node class
 
     public static class Node{
         int data;
         Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        } 
     }
 
-    public static Node head; //head node
+    public static Node head; //head node defined as property
     public static Node tail; //tail node
-
-    public class LinkedList{
-        // Node head;
-        public void insert(int data){
-            Node node = new Node();
-            node.data = data;
-            node.next = null;
-            
-
-        }
-    }
-
 }
