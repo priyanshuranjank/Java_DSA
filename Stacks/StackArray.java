ce64e23d75d950a93102ea92e30d2f932e261e4b
priyanshu3492 <priyanshuranjan1r@gmail.com>
2025-03-19 15:35:19 +0530

    stack

---
+++ Stacks/StackArray.java
@@ -0,0 +1,63 @@
+public class StackArray{ 
+    public static void main(String[] args) {
+     StackArray st = new StackArray();
+        
+     st.push(45);
+     st.push(56);
+     st.push(23);
+    //  st.push(98);
+    //  st.pop();
+    //  st.push(34);
+     st.print();
+     System.out.println("Top element: " + st.peek());
+    // System.out.println(st.pop());
+    // System.out.println();
+    // st.print();
+    System.out.println("Size is: " + st.size());
+    System.out.println(st.isEmpty());
+     
+    }
+
+    
+    public static int stack[] = new int[3];
+    public static int top = 0;
+
+    public void push(int data){
+        if(top == stack.length){
+            System.out.println("Stack is Full.");
+            return;
+        }
+            stack[top] = data;
+            top++;
+        }
+    public void print(){
+            for(int num : stack){
+                System.out.print(num +" ");
+            }
+            System.out.println();
+        }
+    
+    public int pop(){
+        if(top<1){
+            return -1;
+        }
+        top--;
+        int data = stack[top];
+        stack[top] = 0;
+        return data;
+    }
+    public int peek(){
+        if(top<1){
+            return -1;
+        }
+       int data = stack[top-1];
+       return data;
+    }
+    public int size(){
+        return top;
+    }
+    public boolean isEmpty(){
+
+        return top<=0;
+    }
+}
